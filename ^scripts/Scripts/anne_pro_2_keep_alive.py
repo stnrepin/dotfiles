bcdfae9824477e212b6c445303370c1adf522e66
Stepan Repin <stnrepin@gmail.com>
2020-07-23 18:58:01 +0300

    [scripts]: Add script for Anne Pro 2
    This script prevent keyboard suspending

---
+++ ^scripts/Scripts/anne_pro_2_keep_alive.py
@@ -0,0 +1,35 @@
+#!/bin/python
+
+import os
+import time
+from pathlib import Path
+
+ANNE_PRO_2_INPUT_ID_GLOB = 'usb-OBINS_OBINS_AnnePro2_SN*1-event-*'
+SLEEP_TIMEOUT = 1 * 24 * 3600
+
+class AnneProInput(object):
+    def __init__(self, path: Path):
+        self.fo = open(path)
+
+    def close(self):
+        fo.close()
+
+
+def main():
+    pid = os.fork()
+
+    if pid == 0:
+        input_by_id_path = Path('/dev/input/by-id/')
+
+        anne_pro_inputs = []
+        for anne_pro_input_path in input_by_id_path.glob(ANNE_PRO_2_INPUT_ID_GLOB):
+            i = AnneProInput(anne_pro_input_path)
+            anne_pro_inputs.append(i)
+
+        # Waits forever
+        while (not time.sleep(SLEEP_TIMEOUT)):
+            pass
+
+if __name__ == '__main__':
+    main()
+
