e2d5ce9dda9b27b4290bec2df1295162e9e7f745
Stepan Repin <stnrepin@gmail.com>
2022-10-14 18:30:37 +0300

    [nvim]: Show diagnostic code in LSP

--- nvim/.config/nvim/lua/config/lspconfig.lua
+++ nvim/.config/nvim/lua/config/lspconfig.lua
@@ -1,7 +1,12 @@
--- @todo: refactor
+local show_diags_float = function()
+    local fmt = function(diag)
+        return string.format("%s [%s]", diag.message, diag.code or 'no code')
+    end
+    vim.diagnostic.open_float(0, {scope="line",source="always", format=fmt})
+end
 
 local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '<space>e', show_diags_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
