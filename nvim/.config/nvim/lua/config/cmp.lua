a2edf1c0358edd33e82ddef4a0115b9eabe69e94
Stepan Repin <stnrepin@gmail.com>
2023-03-19 20:27:58 +0300

    [nvim]: Fix C-p behavior when using cmp

--- nvim/.config/nvim/lua/config/cmp.lua
+++ nvim/.config/nvim/lua/config/cmp.lua
@@ -10,6 +10,9 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-p>'] = function(fallback)
+            fallback()
+        end
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
