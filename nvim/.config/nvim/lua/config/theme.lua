bffb2d28f62c5ab4f706aa54ad79994f130bbe3a
Stepan Repin <stnrepin@gmail.com>
2024-01-11 20:33:05 +0300

    [tmux]: Fix nvim background color

--- nvim/.config/nvim/lua/config/theme.lua
+++ nvim/.config/nvim/lua/config/theme.lua
@@ -1,10 +1,13 @@
-vim.cmd('colorscheme lucius')
+vim.cmd[[
+    set termguicolors
+    colorscheme lucius
 
-vim.cmd('hi Normal guifg=#d8dee9 guibg=#2e3440 gui=none')
+    hi Normal guifg=#d8dee9 guibg=#2e3440 gui=none
 
-vim.cmd('hi CursorLine guifg=NONE guibg=#3b4252 gui=none')
-vim.cmd('hi SignColumn guifg=#3b4252 guibg=#2e3440 gui=none')
-vim.cmd('hi LineNr guifg=#4c566a guibg=none gui=none')
-vim.cmd('hi CursorLineNr guifg=#616e88 guibg=none gui=none')
+    hi CursorLine guifg=NONE guibg=#3b4252 gui=none
+    hi SignColumn guifg=#3b4252 guibg=#2e3440 gui=none
+    hi LineNr guifg=#4c566a guibg=none gui=none
+    hi CursorLineNr guifg=#616e88 guibg=none gui=none
 
-vim.cmd('hi Visual guifg=NONE guibg=#3b4252 gui=none')
+    hi Visual guifg=NONE guibg=#3b4252 gui=none
+]]
--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -84,4 +84,3 @@ vim.cmd [[
     autocmd BufRead * autocmd FileType <buffer> ++once
       \ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif
 ]]
-
