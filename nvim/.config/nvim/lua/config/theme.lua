8ae886a481eff7fc2a7befdaf5d0972b7c069a18
Stepan Repin <stnrepin@gmail.com>
2024-08-01 00:29:43 +0300

    [nvim]: Fix vertical border color

--- nvim/.config/nvim/lua/config/theme.lua
+++ nvim/.config/nvim/lua/config/theme.lua
@@ -9,6 +9,8 @@ vim.cmd[[
     hi LineNr guifg=#4c566a guibg=none gui=none
     hi CursorLineNr guifg=#616e88 guibg=none gui=none
 
+    hi WinSeparator ctermfg=102 ctermbg=235 guifg=#818698 guibg=#363946
+
     hi Visual guifg=NONE guibg=#3b4252 gui=none
 
     hi Function guifg=#99c1f1
