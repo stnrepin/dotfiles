c28d2c5ab890954ad493f0fa9e9cad2c068759c3
Stepan Repin <stnrepin@gmail.com>
2025-06-01 15:46:14 +0300

    [nvim]: Add telescope grep keymap

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -2,18 +2,19 @@ local telescope = require('telescope')
 local builtins = require('telescope.builtin')
 
 telescope.setup {
-    extensions = {
-        fzf = {
-            fuzzy = true,
-            override_generic_sorter = true,
-            override_file_sorter = true,
-            case_mode = 'smart_case',
-        }
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = 'smart_case',
     }
+  }
 }
 telescope.load_extension('fzf')
 
 vim.cmd[[
-    nnoremap <leader><leader> <cmd>lua require('telescope.builtin').find_files()<CR>
-    nnoremap <leader><CR> <cmd>lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<CR>
+  nnoremap <leader><leader> <cmd>lua require('telescope.builtin').find_files()<CR>
+  nnoremap <leader><CR> <cmd>lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<CR>
+  nnoremap <leader>g <cmd>lua require('telescope.builtin').live_grep()<CR>
 ]]
