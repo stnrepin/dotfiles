61070b5183460dea3a22b8f9e1457a3336c97f93
Stepan Repin <stnrepin@gmail.com>
2025-06-01 17:11:15 +0300

    [nvim]: Fix theme for float windows

--- nvim/.config/nvim/lua/config/theme.lua
+++ nvim/.config/nvim/lua/config/theme.lua
@@ -1,17 +1,19 @@
 vim.cmd[[
-    set termguicolors
-    colorscheme lucius
+  set termguicolors
+  colorscheme lucius
 
-    hi Normal guifg=#d8dee9 guibg=#2e3440 gui=none
+  hi Normal guifg=#d8dee9 guibg=#2e3440 gui=none
 
-    hi CursorLine guifg=NONE guibg=#3b4252 gui=none
-    hi SignColumn guifg=#3b4252 guibg=#2e3440 gui=none
-    hi LineNr guifg=#4c566a guibg=none gui=none
-    hi CursorLineNr guifg=#616e88 guibg=none gui=none
+  hi CursorLine guifg=NONE guibg=#3b4252 gui=none
+  hi SignColumn guifg=#3b4252 guibg=#2e3440 gui=none
+  hi LineNr guifg=#4c566a guibg=none gui=none
+  hi CursorLineNr guifg=#616e88 guibg=none gui=none
 
-    hi WinSeparator ctermfg=102 ctermbg=235 guifg=#818698 guibg=#363946
+  hi WinSeparator ctermfg=102 ctermbg=235 guifg=#818698 guibg=#363946
 
-    hi Visual guifg=NONE guibg=#3b4252 gui=none
+  hi NormalFloat guifg=#e0e0e0 guibg=#303840
 
-    hi Function guifg=#99c1f1
+  hi Visual guifg=NONE guibg=#3b4252 gui=none
+
+  hi Function guifg=#99c1f1
 ]]
