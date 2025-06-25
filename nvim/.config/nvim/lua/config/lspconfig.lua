7b826825926c015b4ed3a97637fc45d08674dfe1
Stepan Repin <stnrepin@gmail.com>
2025-06-26 00:31:00 +0300

    [nvim]: Add keymap for :LspClangdSwitchSourceHeader

--- nvim/.config/nvim/lua/config/lspconfig.lua
+++ nvim/.config/nvim/lua/config/lspconfig.lua
@@ -12,6 +12,7 @@ vim.keymap.set('n', '<space>e', show_diags_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<F10>', ':LspClangdSwitchSourceHeader<CR>', opts)
 
 vim.keymap.del('n', 'grn', {})
 vim.keymap.del('n', 'gra', {})
